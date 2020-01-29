fcdb250ed3b8123a822e07d8c3ced18b9fb6fc2e
Vicky30-max <59610982+Vicky30-max@users.noreply.github.com>
2020-01-29 10:58:40 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,70 @@
+
+'''def add(x,y):
+    z=x+y
+    return z
+def sub(x,y):
+    z=x-y
+    return z
+def mul(x,y):
+    z=x*y
+    return z
+def div(x,y):
+    z=x/y
+    return z
+
+a=int(input("enter 1stnumber:"))
+b=int(input ("enter 2nd number:"))
+ch=int(input("choices 1/2/3/4:"))
+if ch==1:
+     print("The addition is" +  str(add(a,b)))
+elif ch==2:
+    print("the subtraction is"+ str(sub(a,b)))
+elif ch==3:
+     print("the multiplication is"+ str(mul(a,b)))
+elif ch==4:
+     print("the division is"+ str(div(a,b)))
+else: 
+     print("wrong input")'''
+     
+'''for i in "vicky":
+    print ('current word', i)
+    
+name={"vic","vicky","vk"}
+for i in name:
+    print("current name",i)'''
+
+'''for num in range(10,20):
+    for i in range(2,num):
+        if(num%i==0):
+            j=num/i
+            print("its not a prime number" + str(num))
+            break
+    else:
+        print(str(num) + "is a prime number")'''
+        
+
+'''txt = "50"
+i=5
+for i in range(0,5): 
+
+     x = txt.center(10)
+     x1 = txt.center(20)
+     x2 = txt.center(30)
+     x3 = txt.center(40)
+
+print(x)
+print(x1)
+print(x2)
+print(x3)'''
+
+'''x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))'''
+
+'''print(ord('a'))'''
+
+a=2
+if a%2==0:  
+    print("even")
